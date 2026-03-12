6a86a5976dff21b4869b911bec87dccc740a9c2d
Sanan <sananzahid5@gmail.com>
2026-03-13 01:04:38 +0500

    feat: add DatabaseContext with SQLite connection and table initialization (T-14)

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SQLite;
+using LodgeStay.Models;
+using System.Threading.Tasks;
+
+namespace LodgeStay.Data
+{
+    public class DatabaseContext
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public DatabaseContext(string dbpath) 
+        {
+            _database = new SQLiteAsyncConnection(dbpath);
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _database.CreateTableAsync<User>();
+            await _database.CreateTableAsync<Room>();
+            await _database.CreateTableAsync<Reservation>();
+        }
+    }
+}
