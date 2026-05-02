c36f1e5f13d9a2cd99cf5087ce38724a5a8be59e
MunamAnsari <munam.ansari747@gmail.com>
2026-05-02 18:23:41 +0500

    Update ReservationService.cs

--- Services/ReservationService.cs
+++ Services/ReservationService.cs
@@ -1,107 +1,54 @@
-﻿using LodgeStay.Data;
+using LodgeStay.Data;
 using LodgeStay.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace LodgeStay.Services
 {
     public class ReservationService
     {
-        private readonly DatabaseContext _database;
-        private readonly RoomService _roomService;
+        private readonly DatabaseContext _db;
 
-        public ReservationService(DatabaseContext database, RoomService roomService)
+        public ReservationService(DatabaseContext db)
         {
-            _database = database;
-            _roomService = roomService;
+            _db = db;
         }
 
-        public async Task<bool> CreateReservationAsync(Reservation reservation)
+        public Task<List<Reservation>> GetReservationsAsync()
+            => _db.GetAllReservationsAsync();
+
+        public Task<Reservation?> GetReservationByReferenceAsync(string bookingReference)
+            => _db.GetReservationByReferenceAsync(bookingReference);
+
+        public Task UpdateReservationAsync(Reservation reservation)
+            => _db.SaveReservationAsync(reservation);
+
+        public async Task<bool> IsRoomAvailableAsync(int roomId, DateTime checkIn, DateTime checkOut)
         {
-            if (reservation.CheckOut > reservation.CheckIn)
-            {
-                bool isavaiable = await _roomService.IsRoomAvailableAsync(reservation.Room_ID, reservation.CheckIn, reservation.CheckOut);
-                if (isavaiable)
-                {
-                    var room = await _database.GetRoomByIdAsync(reservation.Room_ID);
-                    if (room != null)
-                    {
-                        int nights = (reservation.CheckOut - reservation.CheckIn).Days;
-                        reservation.TotalPrice = nights * room.Price;
-                        await _database.SaveReservationAsync(reservation);
-                        room.Status = "Reserved";
-                        await _database.SaveRoomAsync(room);
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
+            var ci = checkIn.Date;
+            var co = checkOut.Date;
+
+            var overlapping = await _db.GetOverlappingReservationsAsync(ci, co);
+            var conflict = overlapping.Any(r => r.Room_ID == roomId);
+            return !conflict;
         }
 
-        public async Task<bool> CancelReservationAsync(int reservationid)
+        public async Task AddReservationAsync(Reservation reservation)
         {
-            var reservation = await _database.GetReservationByIdAsync(reservationid);
+            reservation.Created_at = DateTime.UtcNow;
+            await _db.SaveReservationAsync(reservation);
+
+            
+        }
+
+        public async Task CancelReservationAsync(string bookingReference)
+        {
+            var reservation = await _db.GetReservationByReferenceAsync(bookingReference);
             if (reservation != null)
             {
                 reservation.Status = "Cancelled";
-                var room = await _database.GetRoomByIdAsync(reservation.Room_ID);
-                if (room == null)
-                {
-                    return false;
-                }
-                room.Status = "Available";
-                await _database.SaveReservationAsync(reservation);
-                await _database.SaveRoomAsync(room);
-                return true;
+                await _db.SaveReservationAsync(reservation);
+
+                
             }
-            else
-            {
-                return false;
-            }
-        }
-
-        public async Task<List<Reservation>> GetAllReservationsAsync()
-        {
-            return await _database.GetAllReservationsAsync();
-        }
-
-        public async Task<Reservation?> GetReservationByIdAsync(int reservationid)
-        {
-            return await _database.GetReservationByIdAsync(reservationid);
-        }
-
-        public async Task<List<Reservation>> GetReservationsByGuestEmailAsync(string email)
-        {
-            return await _database.GetReservationsByGuestEmailAsync(email);
-
-        }
-
-        public async Task<List<Reservation>> GetReservationsAsync()
-        {
-            return await GetAllReservationsAsync();
-        }
-
-        public async Task<bool> AddReservationAsync(Reservation reservation)
-        {
-            return await CreateReservationAsync(reservation);
-        }
-
-        public async Task<List<Reservation>> GetReservationByReferenceAsync(string reference)
-        {
-            var all = await _database.GetAllReservationsAsync();
-            return all.Where(r => r.BookingReference == reference).ToList();
         }
     }
 }
