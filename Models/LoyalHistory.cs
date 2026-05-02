02ddf3c7f651900b371170bf6bc3d4007a6a96ad
MunamAnsari <munam.ansari747@gmail.com>
2026-05-02 18:27:21 +0500

    ft: loyalty points

---
+++ Models/LoyalHistory.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+
+namespace LodgeStay.Models
+{
+    [Table("LoyaltyHistory")]
+    public class LoyaltyHistory
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int GuestId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int Points { get; set; }
+        public bool IsEarn { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
