fb123164fe58c3a7fa889b68d1ced491594e8881
MunamAnsari <munam.ansari747@gmail.com>
2026-05-02 18:26:44 +0500

    ft: Group Booking

---
+++ Models/GroupBooking.cs
@@ -0,0 +1,15 @@
+﻿using LodgeStay.Models;
+
+namespace LodgeStay.Models
+{
+    public class GroupBooking
+    {
+        public string MasterReference { get; set; } = "";
+        public string GroupName { get; set; } = "";
+        public string OrganizerEmail { get; set; } = "";
+        public List<Reservation> Reservations { get; set; } = new();
+        public int DiscountPercent { get; set; }
+        public double TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
